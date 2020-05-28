708e2003308a8b7e7a255d0ba6b860f50297de7f
tulnikov <tulnikov@yandex.ru>
2020-05-28 17:21:14 +0300

    работа с формами часть 1

--- Django/news/models.py
+++ Django/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class News(models.Model):
@@ -13,6 +14,9 @@ class News(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
+
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
@@ -21,6 +25,10 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
+
 
     class Meta:
         verbose_name = 'Категория'
--- Django/news/views.py
+++ Django/news/views.py
@@ -1,29 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.shortcuts import get_object_or_404
 
 from .models import News, Category
+from .forms import NewsForm
 
 # Create your views here.
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
+
     context = {
         'news': news,
         'title': 'Список новостей',
-        'category' : categories,
+
     }
     return render(request,'news/index.html', context)
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
+
     category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request,template_name='news/category.html', context=context)
 
+def view_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
 
+def add_news(request):
+    if request.method == 'POST':
+        pass
+
+    else:
+        form = NewsForm()
+
+    return render(request, 'news/add_news.html', {'form':form})
